a8562cd75ccdfc4815e016236881565303bd687f
Yaroslav <yaroslav.boberskiy@gmail.com>
2015-11-28 16:27:51 +0200

    Task_2.

--- src/SimpleLinkedList.java
+++ src/SimpleLinkedList.java
@@ -6,25 +6,36 @@ public class SimpleLinkedList {
     private Node root;
     private int size;
 
-    public SimpleLinkedList () {
+    public SimpleLinkedList() {
         size = 0;
     }
 
-    public void addFirst (Object obj) {
+    public void addFirst(Object obj) {
 
     }
 
-    public void addLast (Object obj) {
+    public void addLast(Object obj) {
 
     }
 
-    public void addAfter (Object objToAdd, Object objBefore) {
+    public void addAfter(Object objToAdd, Object objBefore) {
 
     }
 
-    public int getSize () {
+    public int getSize() {
 
         return size;
+    }
+
+    public class Node {
+
+        private Object obj;
+        private Node node;
+
+        public Node() {
+
+        }
+
     }
 
 }
