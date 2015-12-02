39d99db0a2e3749d8c43a815c24a3a1ba473d99c
Yaroslav <yaroslav.boberskiy@gmail.com>
2015-12-02 13:49:39 +0200

    Task_7.

--- src/SimpleLinkedList.java
+++ src/SimpleLinkedList.java
@@ -102,6 +102,7 @@ public class SimpleLinkedList implements Iterable<Object> {
                                 cp.nodeNext = null;
                             }
                         }
+
                         size --;
                         break;
                     }
